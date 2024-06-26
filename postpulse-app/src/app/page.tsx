008e343ba2d88c043910fa3dc75d5aa8b4969e3c
Anas SAHEL <anas.sahel@gmail.com>
2024-06-26 07:56:11 +0000

    feat: setup prisma
    - add prisma to the project
    - add prisma schema
    - add prisma client
    - add prisma migrations

--- postpulse-app/src/app/page.tsx
+++ postpulse-app/src/app/page.tsx
@@ -1,25 +1,3 @@
-import { PrismaClient } from "@prisma/client";
-import { headers } from "next/headers";
-
-export default async function Home() {
-  const prisma = new PrismaClient();
-
-  const infos = [];
-
-  const headersIterator = headers().entries();
-  do {
-    infos.push(headersIterator.next().value);
-  } while (!headersIterator.next().done);
-
-  return (
-    <ul>
-      {infos.map((value, index) => {
-        return (
-          <li key={index}>
-            {value["0"]} ---- {value["1"]}
-          </li>
-        );
-      })}
-    </ul>
-  );
+export default function Home() {
+  return <main>Welcome to PostPulse!</main>;
 }
